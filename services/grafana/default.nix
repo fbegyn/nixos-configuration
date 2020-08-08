019154aa16c4f0b363add9b545d03b98f1b9b752
Francis <theci@fom.be>
2020-08-08 13:51:44 +0200

    grafana - bind to all IPs

--- services/grafana/default.nix
+++ services/grafana/default.nix
@@ -1,17 +1,9 @@
 { ... }:
 
-let
-  unstable = import (fetchTarball http://nixos.org/channels/nixos-unstable-small/nixexprs.tar.xz) {};
-in
 {
-  nixpkgs.overlays = [
-    (_self: _super: {
-      grafana = unstable.grafana;
-    })
-  ];
   services.grafana = {
     enable = true;
-    addr = "0.0.0.0";
+    addr = "";
     port = 3000;
     domain = "begyn.lan";
     protocol = "http";
