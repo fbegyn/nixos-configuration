5c4f9ef89946827ba0665c4e4411cc11ff3aea76
Francis Begyn <francis@begyn.be>
2020-10-03 00:29:19 +0200

    fix dump mistake for git identity alias

--- users/francis/configurations/git.nix
+++ users/francis/configurations/git.nix
@@ -7,8 +7,7 @@
       package = pkgs.gitAndTools.gitFull;
       enable = true;
       aliases = {
-        identity = ''
-          "! git config user.name \"$(git config user.$1.name)\"; git config user.email \"$(git config user.$1.email)\"; git config user.signingkey \"$(git config user.$1.signingkey)\"; :"'';
+        identity = "! git config user.name \"$(git config user.$1.name)\"; git config user.email \"$(git config user.$1.email)\"; git config user.signingkey \"$(git config user.$1.signingkey)\"; :";
       };
       extraConfig = {
         core.editor = "vim";
@@ -16,9 +15,6 @@
         user.personal.name = "Francis Begyn";
         user.personal.email = "francis@begyn.be";
         user.personal.signingkey = "";
-        user.robovision.name = "Francis Begyn";
-        user.robovision.email = "francis.begyn@robovision.ai";
-        user.robovision.signingkey = "";
         user.fom.name = "Francis Begyn - Thecy";
         user.fom.email = "theci@fom.be";
         user.fom.signingkey = "";
