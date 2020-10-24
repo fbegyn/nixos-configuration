76714aa5a80013265f68eb5a930bfd5e264ff280
Francis Begyn <francis@begyn.be>
2020-10-25 00:56:48 +0200

    scanner support - maybe

--- hosts/bia/configuration.nix
+++ hosts/bia/configuration.nix
@@ -16,6 +16,7 @@
     ../../common/rtlsdr.nix
     ../../common/fonts.nix
     ../../common/printer.nix
+    ../../common/scanner.nix
     ../../users
     ../../users/francis/gui.nix
     ../../users/francis/configurations/i3
