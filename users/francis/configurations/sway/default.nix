04dc9613d741d1e77bcb257f4248526ea415f063
Francis Begyn <francis@begyn.be>
2020-11-19 00:15:55 +0100

    first attempt to nixify sway config

--- users/francis/configurations/sway/default.nix
+++ users/francis/configurations/sway/default.nix
@@ -7,9 +7,12 @@ let
   startsway = import ./startsway.nix { inherit pkgs; };
 in
 {
-  programs.sway = {
-    enable = true;
-    extraPackages = with pkgs; [
+  programs.waybar.enable = true;
+
+  hardware.opengl.enable = true;
+
+  home-manager.users.francis = {
+    home.packages = with pkgs; [
       unstable.swaylock # lockscreen
       unstable.swayidle
       wf-recorder
@@ -26,77 +29,13 @@ in
       unstable.grim
       unstable.wl-clipboard
       wdisplays
+      unstable.wofi
     ];
-    extraSessionCommands = ''
-      export XDG_SESSION_TYPE=wayland
-      export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-      export QT_AUTO_SCREEN_SCALE_FACTOR=0
-      export QT_SCALE_FACTOR=1
-      export GDK_SCALE=1
-      export GDK_DPI_SCALE=1
-      export MOZ_ENABLE_WAYLAND=1
-      export _JAVA_AWT_WM_NONREPARENTING=1
-    '';
-  };
 
-  systemd.user = {
-    targets.sway-session = {
-      description = "Sway compositor session";
-      documentation = [ "man:systemd.special(7)" ];
-      bindsTo = [ "graphical-session.target" ];
-      wants = [ "graphical-session-pre.target" ];
-      after = [ "graphical-session-pre.target" ];
-    };
-
-    services = {
-      sway = {
-        description = "Sway - Wayland window manager";
-        documentation = [ "man:sway(5)" ];
-        bindsTo = [ "graphical-session.target" ];
-        wants = [ "graphical-session-pre.target" ];
-        after = [ "graphical-session-pre.target" ];
-        environment.PATH = lib.mkForce null;
-        serviceConfig = {
-          Type = "simple";
-          ExecStart = ''
-            ${pkgs.dbus}/bin/dbus-run-session ${pkgs.sway}/bin/sway --debug
-          '';
-          Restart = "on-failure";
-          RestartSec = 1;
-          TimeoutStopSec = 10;
-        };
-      };
-      kanshi = {
-        description = "Kanshi output autoconfig ";
-        wantedBy = [ "graphical-session.target" ];
-        partOf = [ "graphical-session.target" ];
-        serviceConfig = {
-          # kanshi doesn't have an option to specifiy config file yet, so it looks
-          # at .config/kanshi/config
-          ExecStart = ''
-            ${pkgs.kanshi}/bin/kanshi
-          '';
-          RestartSec = 5;
-          Restart = "always";
-        };
-      };
-    };
-  };
-
-  programs.waybar.enable = true;
-
-
-  home-manager.users.francis = {
-    home.packages = with pkgs; [
-    ];
     services.redshift = {
       package = pkgs.unstable.redshift-wlr;
     };
-    programs.fish = {
-      loginShellInit = ''
-        startsway
-      '';
-    };
+
     xdg.configFile = {
       "mako/config".text = ''
         anchor=top-center
@@ -104,42 +43,134 @@ in
       '';
       "waybar/config".source = ./waybar-config;
       "waybar/style.css".source = ./waybar-style.css;
+    };
 
-      "sway/config".text = ''
+    wayland.windowManager.sway = {
+      enable = true;
+      systemdIntegration = true;
+      extraSessionCommands = ''
+        systemctl --user import-environment
+        export XDG_SESSION_TYPE=wayland
+        export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+        export QT_AUTO_SCREEN_SCALE_FACTOR=0
+        export QT_SCALE_FACTOR=1
+        export GDK_SCALE=1
+        export GDK_DPI_SCALE=1
+        export MOZ_ENABLE_WAYLAND=1
+        export _JAVA_AWT_WM_NONREPARENTING=1
+      '';
+      config = {
+        assigns = {
+          "1:web" = [{ class = "^qutebrowser$"; }];
+          "2:comms" = [
+            { class = "^Slack$"; }
+            { class = "^Mattermost$"; }
+            { class = "^Telegram$"; }
+            { class = "^Caprine$"; }
+          ];
+          "3:mail" = [{ class = "^Thunderbird$"; }];
+          "4:music" = [{ class = "^Spotify"; }];
+        };
+        bars = [];
+        floating = {
+          border = 2;
+          criteria = [ ];
+          modifier = "Mod4";
+          titlebar = true;
+        };
+        gaps = {
+          smartGaps = true;
+          inner = 0;
+          outer = 2;
+        };
+        focus = {
+          followMouse = "yes";
+          forceWrapping = false;
+          mouseWarping = true;
+          newWindow = "smart";
+        };
+        menu = "rofi -show drun";
+        fonts = [ "DejaVu Sans Mono 10" ];
+        input = {
+          "1:1:AT_Translated_Set_2_keyboard" = {
+            xkb_layout = "us";
+            xkb_variant = "altgr-intl";
+            xkb_numlock = "disabled";
+          };   
+          "2:10:TPPS/2_Elan_TrackPoint" = {
+            pointer_accel = "-0.17";
+          };
+        };
+        keybindings =
+          let
+            wm = config.home-manager.users.francis.wayland.windowManager.sway;
+            mod = wm.config.modifier;
+            terminal = wm.config.terminal;
+            menu = wm.config.menu;
+          in
+            {
+              "${mod}+Return" = "exec ${terminal}";
+              "${mod}+Shift+Return" = "exec ${terminal} -e tmux new-session -A main";
+        };
+        keycodebindings = { };
+        modes = {
+          resize = {
+            Escape = "mode default";
+            Return = "mode default";
+            h = "resize shrink width 10 px";
+            j = "resize grow height 10 px";
+            k = "resize shrink height 10 px";
+            l = "resize grow width 10 px";
+          };
+        };
+        modifier = "Mod4";
+        startup = [
+          {
+            command = "qutebrowser --qt-flag ignore-gpu-blacklist --qt-flag enable-gpu-rasterization --qt-flag enable-native-gpu-memory-buffers --qt-flag num-raster-threads=2";
+          }
+          {
+            command = "sway-statusbar.sh";
+            always = true;
+          }
+        ];
+        terminal = "alacritty";
+        up = "k";
+        down = "j";
+        right = "l";
+        left = "h";
+        window = {
+          border = 2;
+          commands = [
+            {
+                command = "move to workspace 4:music";
+                criteria = { app_id = "Spotify"; };
+            }
+            {
+                command = "inhibit_idle visible";
+                criteria = { app_id = "mpv"; };
+            }
+            {
+                command = "floating enable";
+                criteria = { app_id = "pavucontrol"; };
+            }
+          ];
+          hideEdgeBorders = "none";
+          titlebar = true;
+        };
+        workspaceAutoBackAndForth = true;
+        workspaceLayout = "default";
+      };
+      extraConfig = ''
         xwayland enable
         # set wallpaper
         output "*" background ~/Pictures/wallpapers/background.jpg fill
         # monitor config
         set $laptop_display eDP-1
         output $laptop_display pos 0 1080 res 1920x1080
-        # configure inputs
-        input 1:1:AT_Translated_Set_2_keyboard {
-          xkb_layout us
-          xkb_variant altgr-intl
-          xkb_numlock disabled
-        }
-
-        input 2:10:TPPS/2_Elan_TrackPoint {
-          pointer_accel -0.17
-        }
-        #
-        #input 1452:570:Toetsenbord_van_Tekenkamer {
-        #    xkb_layout us
-        #    xkb_variant altgr-intl
-        #    xkb_numlock disabled
-        #  }
         # hide cursor after time
         # seat seat0 hide_cursor 2500
         # set global modifier to windows key
         set $mod Mod4
-        # Set the default gaps size for windows
-        gaps inner 1
-        gaps outer 1
-        # Only do gaps when more then 1 container is on the workspace
-        smart_gaps on
-        # Disable the annoying titlebars
-        default_border pixel 2
-        default_floating_border pixel 3
         # Hide border if only 1 window
         hide_edge_borders smart
         # color scheme
@@ -148,23 +179,13 @@ in
         client.unfocused	          #333333	#1c1b19	#888888	#292d2e	#222222
         client.urgent	              #f75341	#ef2f27	#1c1b19	#900000	#f75341
         client.placeholder	        #000000	#0c0c0c	#ffffff	#000000	#0c0c0c
-        # Font for window titles. Will also be used by the bar unless a different font
-        # is used in the bar {} block below.
-        #font pango:monospace 8
-        # This font is widely installed, provides lots of unicode glyphs, right-to-left
-        # text rendering and scalability on retina/hidpi displays (thanks to pango).
-        font pango:DejaVu Sans Mono 8
         # Use Mouse+$mod to drag floating windows to their wanted position
         floating_modifier $mod
         # Set terminal
         set $terminal alacritty
         # start a terminal
-        bindsym $mod+Return exec $terminal
-        bindsym $mod+Shift+Return exec $terminal -e tmux new-session -A -s main
         # kill focused window
         bindsym $mod+Shift+q kill
-        # start dmenu (a program launcher)
-        bindsym $mod+d exec rofi -show drun
         bindsym $mod+a exec rofi -show window
         # change focus
         bindsym $mod+h focus left
@@ -236,38 +257,6 @@ in
         bindsym $mod+Shift+r reload
         # exit i3 (logs you out of your X session)
         bindsym $mod+Shift+e exec "swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your sway session.' -b 'Yes, exit sway' 'swaymsg exit'"
-        # resize window (you can also use the mouse for that)
-        set $size_step 2 px or 2 ppt
-        mode "resize" {
-            # These bindings trigger as soon as you enter the resize mode
-            # Pressing left will shrink the window’s width.
-            # Pressing right will grow the window’s width.
-            # Pressing up will shrink the window’s height.
-            # Pressing down will grow the window’s height.
-            bindsym h resize shrink width $size_step
-            bindsym k resize grow height $size_step
-            bindsym j resize shrink height $size_step
-            bindsym l resize grow width $size_step
-            # same bindings, but for the arrow keys
-            bindsym Left resize shrink width $size_step
-            bindsym Down resize grow height $size_step
-            bindsym Up resize shrink height $size_step
-            bindsym Right resize grow width $size_step
-            # back to normal: Enter or Escape
-            bindsym Return mode "default"
-            bindsym Escape mode "default"
-        }
-        bindsym $mod+r mode "resize"
-        #
-        # Status bar:
-        #
-        exec_always sway-statusbar.sh
-        #bar {
-        #				font pango:DejaVu Sans Mono, FontAwesome 9
-        #        status_command ${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rs/config.toml
-        #        position top
-        #}
-        # Set the locker variable
         set $locker swaylock -c 000000
         #System menu
         set $mode_system System (l)lock,(e)logout,(s)suspend,(r)reboot,(Shift+s)shutdown
@@ -323,8 +312,6 @@ in
         bindsym $mod+Shift+m exec --no-startup-id /home/francis/Scripts/monitor-hotplug.sh
         # Screen lock
         bindsym Mod1+l exec $locker
-        # Autolocker
-        #exec_always --no-startup-id xautolock
         # Volume control
         bindsym XF86AudioLowerVolume exec pulsemixer --change-volume -2
         bindsym XF86AudioRaiseVolume exec pulsemixer --change-volume +2
@@ -341,41 +328,6 @@ in
         bindsym XF86MonBrightnessUp exec --no-startup-id brightnessctl -e s +10%
         # Bind wifi switching script
         bindsym $mod+w exec /home/francis/Scripts/wpa_switcher.sh
-        # Autoswitch workspaces
-        workspace_auto_back_and_forth yes
-        # auto set workspaces
-        #exec --no-startup-id i3-msg "workspace $workspace1; append_layout /home/francis/.config/i3/workspaces/web.json"
-        #exec --no-startup-id $terminal --class 'web_terminal' -t web_terminal -e tmux
-        # Autostart programs
-        #exec --no-startup-id vivaldi-snapshot
-        exec --no-startup-id qutebrowser --qt-flag ignore-gpu-blacklist --qt-flag enable-gpu-rasterization --qt-flag enable-native-gpu-memory-buffers --qt-flag num-raster-threads=2
-        #exec --no-startup-id slack
-        #exec --no-startup-id telegram-desktop
-        #exec --no-startup-id thunderbird
-        #exec --no-startup-id spotify
-        # Asign programs to workspaces
-        assign [class="Hamsket"] $workspace2
-        assign [class="Slack"] $workspace2
-        assign [class="Mattermost"] $workspace2
-        assign [class="Caprine"] $workspace2
-        assign [class="Telegram"] $workspace2
-        assign [class="Thunderbird"] $workspace3
-        assign [class="Mailspring"] $workspace3
-        assign [class="Vivaldi-stable"] $workspace1
-        assign [class="qutebrowser"] $workspace1
-        #assign [class="Firefox"] $workspace1
-        assign [app_id="Spotify"] $workspace4
-        for_window [app_id="Spotify"] move to workspace $workspace4
-        # Set some apps to be floating
-        for_window [app_id="pavucontrol"] floating enable
-        for_window [app_id="Nautilus"] floating enable
-        for_window [app_id="blueberry.py"] floating enable
-        for_window [app_id="Nmtui"] floating enable
-        for_window [app_id="Pulsemixer"] floating enable
-        for_window [app_id="Bluetooth-cli"] floating enable
-        for_window [app_id="m-connection-editor"] floating enable
-        # prevent locking when mpv window is open
-        for_window [app_id="mpv"] inhibit_idle visible
       '';
     };
   };
