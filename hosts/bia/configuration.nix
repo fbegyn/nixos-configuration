b2aa00f89512ee3b4187e47c4a6e2a9b4f23dedf
Francis Begyn <francis@begyn.be>
2020-12-07 23:40:07 +0100

    bia - switch to latest kernel

--- hosts/bia/configuration.nix
+++ hosts/bia/configuration.nix
@@ -27,6 +27,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = [ "ntfs" ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName =
     "bia"; # After the Greek personification of force and raw energy
