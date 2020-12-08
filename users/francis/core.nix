2d3fdd0dda8f1182b1300d95817768b872adafe2
Francis Begyn <francis@begyn.be>
2020-12-08 22:59:53 +0100

    core - pkgs repos moved to folders

--- users/francis/core.nix
+++ users/francis/core.nix
@@ -118,16 +118,5 @@ in {
   nixpkgs.config = {
     allowBroken = true;
     allowUnfree = true;
-
-    packageOverrides = pkgs: {
-      nur = import (builtins.fetchTarball
-        "https://github.com/nix-community/NUR/archive/master.tar.gz") {
-          inherit pkgs;
-        };
-      unstable = import (builtins.fetchTarball
-        "https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz") {
-          config = config.nixpkgs.config;
-        };
-    };
   };
 }
