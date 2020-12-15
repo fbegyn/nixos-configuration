c08a9f71a386dbca16bccdba7a35a6054f493e04
Francis Begyn <francis@begyn.be>
2020-12-15 09:26:25 +0100

    ania - enable docker for work

--- hosts/ania/configuration.nix
+++ hosts/ania/configuration.nix
@@ -105,6 +105,8 @@
     enable = true;
   };
 
+  virtualisation.docker.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
