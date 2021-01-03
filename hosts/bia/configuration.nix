a3dc63678c3cbd56559038711fb1ed55bb5b225b
Francis Begyn <francis@begyn.be>
2021-01-03 18:03:42 +0100

    bia - setup plex and enable firewall

--- hosts/bia/configuration.nix
+++ hosts/bia/configuration.nix
@@ -70,11 +70,18 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  services.plex = {
+    enable = true;
+    openFirewall = true;
+    user = "francis";
+  };
+
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [
+  ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
