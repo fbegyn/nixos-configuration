fe228dbac9c29e076bfa8e96ba1a1337278a1aa9
Francis Begyn <francis@begyn.be>
2021-02-04 00:32:59 +0100

    sway - fix clipboard

--- users/francis/configurations/sway/default.nix
+++ users/francis/configurations/sway/default.nix
@@ -9,6 +9,34 @@ in
 {
   programs.sway.enable = true;
 
+  nixpkgs.overlays = [
+    (self: super: {
+      wl-clipboard-x11 = super.stdenv.mkDerivation rec {
+      pname = "wl-clipboard-x11";
+      version = "5";
+    
+      src = super.fetchFromGitHub {
+        owner = "brunelli";
+        repo = "wl-clipboard-x11";
+        rev = "v${version}";
+        sha256 = "1y7jv7rps0sdzmm859wn2l8q4pg2x35smcrm7mbfxn5vrga0bslb";
+      };
+    
+      dontBuild = true;
+      dontConfigure = true;
+      propagatedBuildInputs = [ super.wl-clipboard ];
+      makeFlags = [ "PREFIX=$(out)" ];
+      };
+    
+      xsel = self.wl-clipboard-x11;
+      xclip = self.wl-clipboard-x11;
+    })
+  ];
+  
+  environment.systemPackages = with pkgs; [
+    wl-clipboard
+  ];
+
   home-manager.users.francis = {
     home.packages = with pkgs; [
       # waybar + scripts
@@ -25,6 +53,7 @@ in
       unstable.slurp
       unstable.grim
       unstable.wl-clipboard
+      unstable.ydotool
       unstable.wofi
       unstable.autotiling
       unstable.gammastep
