1d7ba161f11ce9cca272b63dff99fbf28d230fac
Francis Begyn <francis@begyn.be>
2021-02-06 17:34:20 +0100

    tailscale - setup on desktop

--- hosts/bia/configuration.nix
+++ hosts/bia/configuration.nix
@@ -25,6 +25,8 @@
     ../../users/francis
     ../../users/francis/gui.nix
     ../../users/francis/configurations/i3
+
+    ../../services/tailscale.nix
   ];
 
   # Use the systemd-boot EFI boot loader.
@@ -87,6 +89,36 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  # tailscale machine specific
+  # create a oneshot job to authenticate to Tailscale
+  systemd.services.tailscale-autoconnect = let
+    hosts = import ../../secrets/hosts.nix;
+  in {
+    description = "Automatic connection to Tailscale";
+
+    # make sure tailscale is running before trying to connect to tailscale
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
+
+    # have the job run this shell script
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
+
+      # otherwise authenticate with tailscale
+      ${tailscale}/bin/tailscale up -authkey ${hosts.bia.tailscale.oneoffkey}
+    '';
+  };
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
