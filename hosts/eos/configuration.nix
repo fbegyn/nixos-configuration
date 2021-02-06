bbc0a09b764d77bdf64253ccb795f02ce72e5f94
Francis Begyn <francis@begyn.be>
2021-02-06 17:21:14 +0100

    setup tailscale on the machine

--- hosts/eos/configuration.nix
+++ hosts/eos/configuration.nix
@@ -8,42 +8,55 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    ../../../nixos-hardware/dell/xps/13-9360
+    #../../../nixos-hardware/dell/xps/13-9360
 
     ../../common/base.nix
     ../../common/security.nix
     ../../common/fonts.nix
     ../../common/wireguard.nix
     ../../users
+    ../../users/francis
 
     # services
     ../../services/unifi
     ../../services/coredns
-    ../../services/grafana
-    ../../services/prometheus
+    ../../services/tailscale.nix
   ];
 
   # Use the systemd-boot EFI boot loader.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = [ "ntfs" ];
 
-  networking.hostName = "eos"; # After the Greek titan of dawn
-  networking.wireless = {
-    enable = true;
-    interfaces = [ "wlp58s0" ];
+  # disable the laptop lid switch
+  services.logind.lidSwitch = "ignore";
+  services.logind.lidSwitchDocked = "ignore";
+  services.logind.lidSwitchExternalPower = "ignore";
+
+  networking = {
+    hostName = "eos"; # After the Greek titan of dawn
+    wireless.enable = false;
+    # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+    # Per-interface useDHCP will be mandatory in the future, so this generated config
+    # replicates the default behaviour.
+    useDHCP = false;
+    defaultGateway = {
+      address = "10.5.1.1";
+      interface = "enp57s0u1";
+    };
+    nameservers = [ "1.1.1.1" "8.8.8.8" ];
+    interfaces.enp57s0u1 = {
+      ipv4.addresses = [
+        { address  = "10.5.1.10"; prefixLength = 24; }
+      ];
+    };
+    firewall.enable = false;
   };
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.wlp58s0.useDHCP = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.wireguard = {
+    enable = true;
+  };
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
@@ -55,11 +68,6 @@
   # Set your time zone.
   time.timeZone = "Europe/Brussels";
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = false;
@@ -67,19 +75,42 @@
   };
 
   # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
-  services.openssh.enable = false;
-  services.hardware.bolt.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
-
-  networking.wireguard = {
+  services.openssh = {
     enable = true;
+  };
+  services.hardware.bolt.enable = true;
+  nixpkgs.config.allowUnfree = true;
+
+  # tailscale machine specific
+  # create a oneshot job to authenticate to Tailscale
+  systemd.services.tailscale-autoconnect = let
+    hosts = import ../../secrets/hosts.nix;
+  in {
+    description = "Automatic connection to Tailscale";
+
+    # make sure tailscale is running before trying to connect to tailscale
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
+
+    # have the job run this shell script
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
+
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
+
+      # otherwise authenticate with tailscale
+      ${tailscale}/bin/tailscale up -authkey ${hosts.eos.tailscale.oneoffkey}
+    '';
   };
 
   # This value determines the NixOS release from which the default
@@ -88,6 +119,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.03"; # Did you read the comment?
+  system.stateVersion = "20.09"; # Did you read the comment?
 }
 
---
+++ services/tailscale.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    tailscale
+  ];
+
+  services.tailscale = {
+    enable = true;
+  };
+}
