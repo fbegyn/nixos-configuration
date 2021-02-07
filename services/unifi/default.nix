cf9b41358a17b69f2ec3812bb6bd4769c55e1ddc
Francis Begyn <francis@begyn.be>
2021-02-07 16:55:25 +0100

    eos - set up some basic monitoring on the network

--- services/unifi/default.nix
+++ services/unifi/default.nix
@@ -1,8 +1,18 @@
 { pkgs, ... }:
 
-{
+let
+  vars = import ../../secrets/variables.nix;
+in {
   services.unifi = {
     enable = true;
     unifiPackage = pkgs.unstable.unifi;
+  };
+
+  services.prometheus.exporters.unifi = {
+    enable = true;
+    unifiAddress = "${vars.unifi.host}";
+    unifiUsername = "${vars.unifi.username}";
+    unifiPassword = "${vars.unifi.password}";
+    unifiInsecure = true;
   };
 }
