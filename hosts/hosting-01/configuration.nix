9ae12865d19feaf4f5fd18b1de93633407a7b9f0
Francis Begyn <francis@begyn.be>
2021-04-29 13:05:28 +0200

    update hosting server node and connect tailscale

--- hosts/hosting-01/configuration.nix
+++ hosts/hosting-01/configuration.nix
@@ -13,28 +13,25 @@
       ../../common/master.nix
       ../../common/unstable.nix
       ../../common/security.nix
+      ../../common/system.nix
+      ../../common/resolved.nix
 
       ../../users
       ../../users/francis
 
       ../../common/acme.nix
       ../../services/website
+      ../../services/tailscale.nix
     ];
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-  networking.hostName = "host-02"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Set your time zone.
-  time.timeZone = "Europe/Brussels";
+  networking.hostName = "hosting-01"; # Define your hostname.
+  time.timeZone = "Europe/Brussels"; # Set your time zone.
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
@@ -43,51 +40,36 @@
   networking.interfaces.ens10.useDHCP = true;
   networking.interfaces.ens3.useDHCP = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # tailscale machine specific
+  # create a oneshot job to authenticate to Tailscale
+  systemd.services.tailscale-autoconnect = let
+    hosts = import ../../secrets/hosts.nix;
+  in {
+    description = "Automatic connection to Tailscale";
 
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  # };
+    # make sure tailscale is running before trying to connect to tailscale
+    after = [ "network-pre.target" "tailscale.service" ];
+    wants = [ "network-pre.target" "tailscale.service" ];
+    wantedBy = [ "multi-user.target" ];
 
-  
+    # set this service as a oneshot job
+    serviceConfig.Type = "oneshot";
 
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
+    # have the job run this shell script
+    script = with pkgs; ''
+      # wait for tailscaled to settle
+      sleep 2
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+      # check if we are already authenticated to tailscale
+      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+      if [ $status = "Running" ]; then # if so, then do nothing
+        exit 0
+      fi
 
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.jane = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  # };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
+      # otherwise authenticate with tailscale
+      ${tailscale}/bin/tailscale up -authkey ${hosts.hosting-01.tailscale.oneoffkey}
+    '';
+  };
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
