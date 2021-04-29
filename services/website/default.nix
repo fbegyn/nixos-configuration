2929ea03f694f1fcb5a3e4265d240288a5f49610
Francis Begyn <francis@begyn.be>
2021-04-29 11:21:15 +0200

    update website links

--- services/website/default.nix
+++ services/website/default.nix
@@ -15,15 +15,33 @@
   };
 
   services.nginx.enable = true;
+  # francis.begyn.be website/blog
   services.nginx.virtualHosts."francis.begyn.be" = {
     forceSSL = true;
     enableACME = true;
     root = "/var/www/francis.begyn.be";
     locations."/" = {
       proxyPass = "http://localhost:3114";
-    }; 
+      extraConfig = ''
+        add_header Permissions-Policy interest-cohort=();
+      '';
+    };
   };
   security.acme.certs."francis.begyn.be".email = "francis.begyn+website@gmail.com";
+
+  # francis.begyn.eu website/blog - redirect
+  services.nginx.virtualHosts."francis.begyn.eu" = {
+    forceSSL = true;
+    enableACME = true;
+    root = "/var/www/francis.begyn.eu";
+    locations."/" = {
+      proxyPass = "http://localhost:3114";
+      extraConfig = ''
+        add_header Permissions-Policy interest-cohort=();
+      '';
+    };
+  };
+  security.acme.certs."francis.begyn.eu".email = "francis.begyn+website@gmail.com";
 
   networking.firewall = {
     allowedTCPPorts = [
