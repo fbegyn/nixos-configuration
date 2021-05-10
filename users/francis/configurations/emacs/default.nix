301105bc934d1c89f4426d5371a1572e0a3ee084
Francis Begyn <francis@begyn.be>
2021-05-10 10:32:11 +0200

    attempt to fix emacs opacity

--- users/francis/configurations/emacs/default.nix
+++ users/francis/configurations/emacs/default.nix
@@ -106,8 +106,8 @@ in
             (global-display-line-numbers-mode))
 
         ;; tweak some parameters
-        (set-frame-parameter (selected-frame) 'alpha '(85 . 85))
-        (add-to-list 'default-frame-alist '(alpha . (85 . 85)))
+        (set-frame-parameter (selected-frame) 'alpha '(100 . 90))
+        (add-to-list 'default-frame-alist '(alpha . (100 . 90)))
 
         ;; go
         (setenv "GOPATH" (concat (getenv "HOME") "/go"))
