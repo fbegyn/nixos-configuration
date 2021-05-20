fac25a1424ec16e04c3e9d77c231c53529883a15
Francis Begyn <francis@begyn.be>
2021-05-20 19:33:26 +0200

    znc - switching around the clients

--- hosts/horme/configuration.nix
+++ hosts/horme/configuration.nix
@@ -85,20 +85,15 @@
   # Or disable the firewall altogether.
   networking.firewall.enable = false;
 
+  thecy.services.tailscale.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
   programs.adb.enable = true;
   services.hardware.bolt.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.09"; # Did you read the comment?
-
+  # system backups
   services.restic.backups = {
     gdrive = {
       user = "francis";
@@ -111,5 +106,13 @@
       };
     };
   };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
 }
 
