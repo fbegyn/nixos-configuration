0b827491668c0c17770370586c89a8c7e8a76f3b
Francis Begyn <francis@begyn.be>
2021-05-20 23:44:28 +0200

    small fixes

--- hosts/eos/configuration.nix
+++ hosts/eos/configuration.nix
@@ -96,34 +96,14 @@
   services.hardware.bolt.enable = true;
 
   # tailscale machine specific
-  # create a oneshot job to authenticate to Tailscale
-  systemd.services.tailscale-autoconnect = let
+  thecy.services.tailscale = let
     hosts = import ../../secrets/hosts.nix;
   in {
-    description = "Automatic connection to Tailscale";
-
-    # make sure tailscale is running before trying to connect to tailscale
-    after = [ "network-pre.target" "tailscaled.service" ];
-    wants = [ "network-pre.target" "tailscaled.service" ];
-    wantedBy = [ "multi-user.target" ];
-
-    # set this service as a oneshot job
-    serviceConfig.Type = "oneshot";
-
-    # have the job run this shell script
-    script = with pkgs.unstable; ''
-      # wait for tailscaled to settle
-      sleep 2
-
-      # check if we are already authenticated to tailscale
-      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then # if so, then do nothing
-        exit 0
-      fi
-
-      # otherwise authenticate with tailscale
-      ${tailscale}/bin/tailscale up -authkey ${hosts.eos.tailscale.oneoffkey}
-    '';
+    enable = true;
+    autoprovision = {
+      enable = true;
+      key = "${hosts.eos.tailscale.oneoffkey}";
+    };
   };
 
   services.ddclient = {
--- users/default.nix
+++ users/default.nix
@@ -26,6 +26,7 @@
     shell = pkgs.fish;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKjrK5P5gwQYt4GOxGtGoHk6Yj8vMVk7Sp9X6EXOWo4i francis@horme"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFwnEB+L6Kk7QNXsQblBbavrd2bdmIRxGPKzZY7n+FnB francis@bia"
     ];
   };
 
