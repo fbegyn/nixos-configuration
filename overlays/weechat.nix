7ba6d1686e4d357d69c539ac017770aeb8fa4436
Francis Begyn <francis@begyn.be>
2021-05-25 14:23:14 +0200

    weechat - fix sso forced login

--- overlays/weechat.nix
+++ overlays/weechat.nix
@@ -2,6 +2,7 @@ self: super: {
   weechatScripts.weechat-matrix-fixed = super.weechatScripts.weechat-matrix.overrideAttrs (oldAttrs: rec {
     postFixup = oldAttrs.postFixup + ''
       substituteInPlace $out/lib/*/site-packages/matrix/server.py --replace "\"matrix_sso_helper\"" "\"$out/bin/matrix_sso_helper\""
+      substituteInPlace $out/lib/*/site-packages/matrix/server.py --replace "and (not self.config.username or self.config.password)):" "and (not self.config.username or not self.config.password)):"
     '';  
   });
 
