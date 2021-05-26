82d17af9bb23ac0ff15f46f1b9aaca5687ca0256
Francis Begyn <francis@begyn.be>
2021-05-26 02:54:05 +0200

    pkgs - remove master packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,11 +3,6 @@ pkgs: rec {
     "https://github.com/nix-community/NUR/archive/master.tar.gz") {
       inherit pkgs;
     };
-  master = import (builtins.fetchTarball
-    "https://github.com/NixOS/nixpkgs/archive/master.tar.gz") {
-      inherit pkgs;
-      config.allowUnfree = true;
-    };
   unstable = import (builtins.fetchTarball
     "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {
       inherit pkgs;
