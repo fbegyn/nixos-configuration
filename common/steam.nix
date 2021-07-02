c5a6da1e14efc219dd23bdf94a11efa970594879
Francis Begyn <francis@begyn.be>
2021-07-03 00:24:35 +0200

    switch to steam options

--- common/steam.nix
+++ common/steam.nix
@@ -2,16 +2,6 @@
 
 {
   # make steam work
-  hardware.opengl = {
-    enable = true;
-    driSupport32Bit = true;
-    extraPackages32 = with pkgs.pkgsi686Linux; [ libva  ];
-  };
-  hardware.pulseaudio.support32Bit = true;
-  home-manager.users.francis = {
-    home.packages = with pkgs; [
-      steam
-    ];
-  };
+  programs.steam.enable = true;
 }
 
