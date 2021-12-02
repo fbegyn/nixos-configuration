00b3f1f1741c915e986916f9b79f84ee48d92b48
Francis Begyn <francis@begyn.be>
2021-12-02 10:45:56 +0100

    hidpi - disable

--- common/hidpi.nix
+++ common/hidpi.nix
@@ -1,5 +1,14 @@
 { configs, pkgs, ... }:
 
 {
-  hardware.video.hidpi.enable = true;
+  hardware.video.hidpi.enable = false;
+  # services.xserver.dpi = 180;
+  # environment.variables = {
+  #   _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
+  #   GDK_SCALE = "1";
+  #   GDK_DPI_SCALE = "0.5";
+  #   # QT_AUTO_SCREEN_SET_FACTOR = "1";
+  #   # QT_SCALE_FACTOR = "1";
+  #   # QT_FONT_DPI = "32";
+  # };
 }
