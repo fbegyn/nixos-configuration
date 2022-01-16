3c2b80e1896261f206b2d3bb40e500622a93a233
Francis Begyn <francis@begyn.be>
2022-01-16 16:55:35 +0100

    moonlander - add wally

--- common/moonlander.nix
+++ common/moonlander.nix
@@ -1,6 +1,9 @@
 { pkgs, lib, writeTextFile, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    wally-cli
+  ];
   services.udev.extraRules = ''
     # Teensy rules for the Ergodox EZ
     ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
