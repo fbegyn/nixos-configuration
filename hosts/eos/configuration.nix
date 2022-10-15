edd15e58fe032776e656842d1f57f6f5b9b8ef56
Francis Begyn <francis@begyn.be>
2022-10-15 14:13:54 +0200

    eos - add nextcloud DB and nextcloud boot order

--- hosts/eos/configuration.nix
+++ hosts/eos/configuration.nix
@@ -198,10 +198,14 @@ in {
   };
 
   services.postgresql = {
-    ensureDatabases = [];
+    enable = true;
+    ensureDatabases = [ "nextcloud" ];
     ensureUsers = [
+      {
+        name = "nextcloud";
+        ensurePermissions."DATABASE nextcloud" = "ALL PRIVILEGES";
+      }
     ];
-    enableTCPIP = true;
   };
 
   services.consul = {
@@ -371,6 +375,6 @@ in {
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.09"; # Did you read the comment?
+  system.stateVersion = "22.05"; # Did you read the comment?
 }
 
