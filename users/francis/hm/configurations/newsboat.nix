893a0217794f9b8339c61f7e9d32a10939b217d1
Francis Begyn <francis@begyn.be>
2022-11-29 12:48:04 +0100

    newsboat - add feeds

--- users/francis/hm/configurations/newsboat.nix
+++ users/francis/hm/configurations/newsboat.nix
@@ -6,9 +6,11 @@
       { url = http://sreweekly.com/feed/; }
       { url = https://blog.cloudflare.com/rss/; }
       { url = https://news.ycombinator.com/rss; }
-      { url = https://www.vrt.be/vrtnws/nl.rss.headlines.xml; }
-      { url = https://www.ugent.be/RSS; }
+      { url = https://www.vrt.be/vrtnws/nl.rss.headlines.xml; titel = "VRT NWS"; }
+      { url = https://www.ugent.be/RSS; title = "UGent RSS"; }
       { url = https://francis.begyn.be/blog.rss; }
+      { url = https://blog.codinghorror.com/rss/; }
+      { url = https://predr.ag/atom.xml; }
     ];
     autoReload = true;
     reloadTime = 300;
