b50a48d1e4788856331f3ce3685e9bcb7e666ead
Francis Begyn <francis@begyn.be>
2023-02-24 00:36:05 +0100

    remove matrix patch

--- overlays/weechat.nix
+++ overlays/weechat.nix
@@ -1,17 +1,11 @@
-self: super: {
-  weechatScripts.weechat-matrix-fixed = super.weechatScripts.weechat-matrix.overrideAttrs (oldAttrs: rec {
-    postFixup = oldAttrs.postFixup + ''
-      substituteInPlace $out/lib/*/site-packages/matrix/server.py --replace "\"matrix_sso_helper\"" "\"$out/bin/matrix_sso_helper\""
-      substituteInPlace $out/lib/*/site-packages/matrix/server.py --replace "and (not self.config.username or self.config.password)):" "and (not self.config.username or not self.config.password)):"
-    '';
-  });
-
-  weechat =  with super.weechatScripts; super.weechat.override {
+self: surer: {
+  weechat =  with super.weechatScripts; {
     configure = { availablePlugins, ... }: {
-      scripts =
-        [ multiline ];
-      extraBuildInputs =
-        [ availablePlugins.python.withPackages (_: [ weechat-matrix ])];
+      scripts = [
+        multiline
+        weechat-matrix
+      ];
+      extraBuildInputs = [];
     };
   };
 }
