89d1410a6dd267114db8ca08559483462cb01e00
Francis Begyn <francis@begyn.be>
2023-03-21 13:14:49 +0100

    remove duplicate mounts

--- hosts/ania/configuration.nix
+++ hosts/ania/configuration.nix
@@ -53,21 +53,6 @@
   boot.loader.grub.copyKernels = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.zfsSupport = true;
-  boot.loader.grub.extraPrepareConfig = ''
-  mkdir -p /boot/efis
-  for i in  /boot/efis/*; do mount $i ; done
-
-  mkdir -p /boot/efi
-  mount /boot/efi
-  '';
-  boot.loader.grub.extraInstallCommands = ''
-  ESP_MIRROR=$(mktemp -d)
-  cp -r /boot/efi/EFI $ESP_MIRROR
-  for i in /boot/efis/*; do
-   cp -r $ESP_MIRROR/EFI $i
-  done
-  rm -rf $ESP_MIRROR
-  '';
   boot.loader.grub.devices = [
     "/dev/disk/by-id/nvme-eui.e8238fa6bf530001001b448b4b64ab08"
   ];
