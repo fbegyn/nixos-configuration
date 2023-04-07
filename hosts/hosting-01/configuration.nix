e0f0b2f36774b4352c39d56bbc3a92c9de663add
Francis Begyn <francis@begyn.be>
2023-04-07 18:32:52 +0200

    unifi - switch to supported DB version

--- hosts/hosting-01/configuration.nix
+++ hosts/hosting-01/configuration.nix
@@ -71,7 +71,7 @@
     enable = true;
     unifiPackage = pkgs.unstable.unifi;
     jrePackage = pkgs.jdk11;
-    mongodbPackage = pkgs.mongodb-6_0;
+    mongodbPackage = pkgs.mongodb-3_6;
     openFirewall = true;
   };
   services.nginx.virtualHosts = {
