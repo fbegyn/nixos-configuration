7759d958f4018dff203423a34a4197948704490b
Francis Begyn <francis@begyn.be>
2023-11-23 08:41:26 +0100

    (erebus) patch emacs with mac adjustments

--- hosts/erebus/configuration.nix
+++ hosts/erebus/configuration.nix
@@ -110,7 +110,37 @@
       vimAlias = true;
       vimdiffAlias = true;
     };
+    emacs.emacsPackage = pkgs.emacs-pgtk.overrideAttrs (old: {
+      patches = (old.patches or []) ++ [
+        # Fix OS window role (needed for window managers like yabai)
+        (pkgs.fetchpatch {
+          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
+          sha256 = "sha256-+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE";
+        })
+        # Use poll instead of select to get file descriptors
+        (pkgs.fetchpatch {
+          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/poll.patch";
+          sha256 = "sha256-jN9MlD8/ZrnLuP2/HUXXEVVd6A+aRZNYFdZF8ReJGfY";
+        })
+        # Enable rounded window with no decoration
+        (pkgs.fetchpatch {
+          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/round-undecorated-frame.patch";
+          sha256 = "sha256-uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4";
+        })
+        # Make Emacs aware of OS-level light/dark mode
+        (pkgs.fetchpatch {
+          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/system-appearance.patch";
+          sha256 = "sha256-oM6fXdXCWVcBnNrzXmF0ZMdp8j0pzkLE66WteeCutv8";
+        })
+        # Window refocus
+        (pkgs.fetchpatch {
+          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/no-frame-refocus-cocoa.patch";
+          sha256 = "sha256-QLGplGoRpM4qgrIAJIbVJJsa4xj34axwT3LiWt++j/c";
+        })
+      ];
+    });
   };
+
 
   # nix settings
   nixpkgs.hostPlatform = "aarch64-darwin";
