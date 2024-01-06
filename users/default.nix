ba3028dd8bf95caccc2a6598ee7d6edd9d8126a6
Francis Begyn <francis@begyn.be>
2024-01-06 03:49:53 +0100

    some random tweaks

--- users/default.nix
+++ users/default.nix
@@ -2,8 +2,7 @@
 
 {
   users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHGwhmmWLgfOpsv1U4bhVqc95jBXK9KpuDjJxSYGiMzE francis@horme"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFwnEB+L6Kk7QNXsQblBbavrd2bdmIRxGPKzZY7n+FnB francis@bia"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINK7mMVKOmELe+FVvn1oWNRwKiANgTwcnzte3vWK3nMV"
     "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLQRocoQMC4JDWPL7fYYCfhpq5SoUfaCxUeQpgQrZ7c4Q44/YWPMjHkmGM8+7ZehfVHVxSNJ4i/Rre1n0MJcCT8= macbook-francis@secretive.erebus.local"
   ];
 
@@ -32,8 +31,7 @@
     group = "francis";
     shell = pkgs.unstable.fish;
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHGwhmmWLgfOpsv1U4bhVqc95jBXK9KpuDjJxSYGiMzE francis@horme"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFwnEB+L6Kk7QNXsQblBbavrd2bdmIRxGPKzZY7n+FnB francis@bia"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINK7mMVKOmELe+FVvn1oWNRwKiANgTwcnzte3vWK3nMV"
       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLQRocoQMC4JDWPL7fYYCfhpq5SoUfaCxUeQpgQrZ7c4Q44/YWPMjHkmGM8+7ZehfVHVxSNJ4i/Rre1n0MJcCT8= macbook-francis@secretive.erebus.local"
     ];
   };
