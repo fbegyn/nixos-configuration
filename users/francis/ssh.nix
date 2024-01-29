2b7b6be7dc204bcd39226c61af77bb32e1dd5d86
Francis Begyn <francis@begyn.be>
2024-01-29 11:56:04 +0100

    (ssh) switch too using the ssh-agent

--- users/francis/ssh.nix
+++ users/francis/ssh.nix
@@ -1,6 +1,10 @@
 { config, pkgs, ... }:
 
-{
+let
+  authPath = "/run/user/1000/ssh-agent";
+in {
+  environment.variables.SSH_AUTH_SOCK = "${authPath}";
+  environment.sessionVariables.SSH_AUTH_SOCK = "${authPath}";
   programs = {
     ssh = {
       startAgent = true;
@@ -10,9 +14,4 @@
       enableSSHSupport = false;
     };
   };
-
-  # environment.shellInit = ''
-  #   gpg-connect-agent /bye
-  #   export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-  # '';
 }
