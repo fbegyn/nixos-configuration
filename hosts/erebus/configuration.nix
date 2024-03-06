0a8481f9ee737f697c7b289998368283c2ce2b07
Francis Begyn <francis@begyn.be>
2024-03-06 21:56:44 +0100

    (macos) temp disable some patches

--- hosts/erebus/configuration.nix
+++ hosts/erebus/configuration.nix
@@ -132,10 +132,10 @@
     emacs.emacsPackage = pkgs.unstable.emacs29-pgtk.overrideAttrs (old: {
       patches = (old.patches or []) ++ [
         # Fix OS window role (needed for window managers like yabai)
-        (pkgs.fetchpatch {
-          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/fix-window-role.patch";
-          sha256 = "sha256-+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE";
-        })
+        # (pkgs.fetchpatch {
+        #   url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/fix-window-role.patch";
+        #   sha256 = "sha256-+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE";
+        # })
         # Use poll instead of select to get file descriptors
         (pkgs.fetchpatch {
           url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/poll.patch";
@@ -147,10 +147,10 @@
           sha256 = "sha256-uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4";
         })
         # Make Emacs aware of OS-level light/dark mode
-        (pkgs.fetchpatch {
-          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/system-appearance.patch";
-          sha256 = "sha256-oM6fXdXCWVcBnNrzXmF0ZMdp8j0pzkLE66WteeCutv8";
-        })
+        # (pkgs.fetchpatch {
+        #   url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/system-appearance.patch";
+        #   sha256 = "sha256-oM6fXdXCWVcBnNrzXmF0ZMdp8j0pzkLE66WteeCutv8";
+        # })
       ];
     });
   };
