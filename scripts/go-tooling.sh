eba991788630b78c8b833fc2312a1051f557d8a8
Francis Begyn <francis@begyn.be>
2024-03-29 21:30:15 +0100

    (go-tooling) add gofumpt

--- scripts/go-tooling.sh
+++ scripts/go-tooling.sh
@@ -5,4 +5,5 @@ go install -v github.com/davidrjenni/reftools/cmd/fillstruct@latest
 go install -v golang.org/x/tools/gopls@latest
 go install -v golang.org/x/tools/cmd/goimports@latest
 go install -v github.com/goreleaser/goreleaser@latest
+go install -v mvdan.cc/gofumpt@latest
 
