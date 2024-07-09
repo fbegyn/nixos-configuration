ba2863742e89a241ffef6928b26e9ead32f54750
Francis Begyn <francis@begyn.be>
2024-07-09 12:29:00 +0200

    (alacritty) set theme for alacritty through hm

--- users/francis/hm/base.nix
+++ users/francis/hm/base.nix
@@ -1,11 +1,24 @@
-{ config, pkgs, ... }:
+{ config, pkgs, nix-colors, ... }:
 
 {
   imports = [
     ./configurations/git.nix
     ./configurations/fish
     ./configurations/bash.nix
+    nix-colors.homeManagerModules.default
   ];
+
+  # colorScheme = let
+  #   colorscheme = "base16/gruvbox-light-medium";
+  #   src =
+  #     "https://raw.githubusercontent.com/tinted-theming/schemes/spec-0.11/${colorscheme}.yaml";
+  #   theme = nix-colors.lib.schemeFromYAML builtins.readFile ( builtins.fetchurl {
+  #     url = "${src}";
+  #     sha256 = "sha256:1gqnn8wpjwqyn33r3s69xgappgf7z76wlnf4asi4yn4l32a5z4ks";
+  #   });
+  # in theme;
+
+  colorScheme = nix-colors.colorSchemes.base16.gruvbox-light-medium;
 
   xdg.configFile = {
     "nixpkgs/config.nix".source = ./configurations/nixpkgs-config.nix;
--- users/francis/hm/configurations/alacritty/default.nix
+++ users/francis/hm/configurations/alacritty/default.nix
@@ -1,10 +1,66 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
-  home.packages = with pkgs.unstable; [
-    alacritty
-  ];
-  xdg.configFile = {
-    "alacritty/alacritty.yml".source = ./alacritty.yml;
+  programs.alacritty = {
+    enable = true;
+    package = pkgs.unstable.alacritty;
+    settings = {
+      live_config_reload = true;
+      bell.duration = 0;
+      cursor.unfocused_hollow = true;
+      env.TERM = "xterm-256color";
+      font = {
+        size = 10;
+        glyph_offset = {
+          x = 0;
+          y = 0;
+        };
+        offset = {
+          x = 0;
+          y = 0;
+        };
+        normal = {
+          family = "DejaVu Sans Mono";
+          style = "Regular";
+        };
+      };
+      mouse = {
+        hide_when_typing = true;
+        bindings = [{
+          action = "PasteSelection";
+          mouse = "Middle";
+        }];
+      };
+      selection.semantic_escape_chars = ",|`|:\"' ()[]{}<>";
+      window = {
+        decorations = "full";
+        dynamic_title = true;
+        opacity = 0.9;
+        dimensions = {
+          columns = 0;
+          lines = 0;
+        };
+        padding = {
+          x = 2;
+          y = 2;
+        };
+      };
+      colors = {
+        primary = {
+          foreground = "#${config.colorScheme.palette.base05}";
+          background = "#${config.colorScheme.palette.base00}";
+        };
+        normal = {
+          black = "#1c1b19";
+          blue = "#${config.colorScheme.palette.base0D}";
+          cyan = "#${config.colorScheme.palette.base0C}";
+          green = "#${config.colorScheme.palette.base0B}";
+          magenta = "#${config.colorScheme.palette.base0E}";
+          red = "#${config.colorScheme.palette.base08}";
+          yellow = "#${config.colorScheme.palette.base0A}";
+          white = "#d0bfa1";
+        };
+      };
+    };
   };
 }
