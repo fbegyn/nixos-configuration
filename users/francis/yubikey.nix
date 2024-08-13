656a9197b1e9d058bbed1a813773b226d0d3633a
Francis Begyn <francis@begyn.be>
2024-08-13 09:41:17 +0200

    (yubico) - add some more tools for the keys

--- users/francis/yubikey.nix
+++ users/francis/yubikey.nix
@@ -6,11 +6,14 @@
     paperkey
     yubico-pam
     yubikey-manager
+    yubikey-manager-qt
     yubikey-personalization
+    yubikey-personalization-gui
   ];
 
   services.pcscd.enable = true;
   services.udev.packages = with pkgs.unstable; [
+    yubikey-manager
     yubikey-personalization
   ];
 
