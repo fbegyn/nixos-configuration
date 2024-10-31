5f6a8c2c1824f7c82495ce4be2c6d098a97cd39c
Francis Begyn <francis@begyn.be>
2024-10-31 12:32:40 +0100

    fix bash fish startup through home-manager
    Signed-off-by: Francis Begyn <francis@begyn.be>

--- users/francis/hm/configurations/bash.nix
+++ users/francis/hm/configurations/bash.nix
@@ -31,13 +31,6 @@
       MOZ_ENABLE_WAYLAND = 1;
       _JAVA_AWT_WM_NONREPARENTING = 1;
     };
-    interactiveShellInit = ''
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-        then
-          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-          exec ${config.home-manager.users.francis.programs.fish.package}/bin/fish $LOGIN_OPTION
-      fi
-    '';
     initExtra = let
       gitPrompt = builtins.fetchurl {
         url = "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh";
@@ -48,6 +41,12 @@
         sha256 = "106wrn2wspci19a70006g5xsh679ap2973h2lmssf5xbl3r3lv7g";
       };
     in ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+        then
+          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+          exec ${config.programs.fish.package}/bin/fish $LOGIN_OPTION
+      fi
+
       # load git-prompt script
       . ${gitPrompt}
       # load git completions
