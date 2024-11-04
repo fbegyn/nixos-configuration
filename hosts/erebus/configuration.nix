929aec01f4653224d5022138d54368abbb825c93
Francis Begyn <francis@begyn.be>
2024-11-04 08:59:47 +0100

    (mac) update emacs and mac-darwin settings
    Signed-off-by: Francis Begyn <francis@begyn.be>

--- hosts/erebus/configuration.nix
+++ hosts/erebus/configuration.nix
@@ -22,7 +22,6 @@
       bashInteractive
       fish
     ];
-    loginShell = pkgs.bash;
     systemPath = [
       "/Users/francis/.local/bin"
       "/Users/francis/.go/bin"
@@ -201,26 +200,28 @@
       elixir-ls
     ];
 
-    emacs.emacsPackage = pkgs.unstable.emacs29-pgtk.overrideAttrs (old: {
+    emacs.emacsPackage = pkgs.unstable.emacs29-gtk3.overrideAttrs (old: {
       patches = (old.patches or []) ++ [
         # Fix OS window role (needed for window managers like yabai)
         (pkgs.fetchpatch {
-          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/fix-window-role.patch";
+          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-30/fix-window-role.patch";
           sha256 = "sha256-+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE";
         })
         # Use poll instead of select to get file descriptors
-        (pkgs.fetchpatch {
-          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/poll.patch";
-          sha256 = "sha256-jN9MlD8/ZrnLuP2/HUXXEVVd6A+aRZNYFdZF8ReJGfY";
-        })
+        # TODO: poll patch removed from upstream, follow up on it
+        # https://github.com/d12frosted/homebrew-emacs-plus/pull/746
+        # (pkgs.fetchpatch {
+        #   url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/poll.patch";
+        #   sha256 = "sha256-jN9MlD8/ZrnLuP2/HUXXEVVd6A+aRZNYFdZF8ReJGfY";
+        # })
         # Enable rounded window with no decoration
         (pkgs.fetchpatch {
-          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/round-undecorated-frame.patch";
+          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-30/round-undecorated-frame.patch";
           sha256 = "sha256-uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4";
         })
         # Make Emacs aware of OS-level light/dark mode
         (pkgs.fetchpatch {
-          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/system-appearance.patch";
+          url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-30/system-appearance.patch";
           sha256 = "sha256-oM6fXdXCWVcBnNrzXmF0ZMdp8j0pzkLE66WteeCutv8";
         })
       ];
