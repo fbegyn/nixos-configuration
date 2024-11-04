6f0821c71b11bb3a6c8cdd9d5a04a5fb96e26353
Francis Begyn <francis@begyn.be>
2024-11-04 09:20:44 +0100

    (ania) try out emacs30-gtk3 on this device
    Signed-off-by: Francis Begyn <francis@begyn.be>

--- hosts/ania/configuration.nix
+++ hosts/ania/configuration.nix
@@ -131,14 +131,14 @@
   };
   services.irqbalance.enable = true;
 
-  home-manager.users.francis.home.stateVersion = "23.05";
+  home-manager.users.francis.home.stateVersion = "24.05";
+  home-manager.users.francis.emacs.emacsPackage = pkgs.unstable.emacs30-gtk3;
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?
-
+  system.stateVersion = "24.05"; # Did you read the comment?
 }
 
