9902342b907f815851b9a84ed3c2dbdf62a8fbd3
Francis Begyn <francis@begyn.be>
2024-12-29 22:18:05 +0100

    (git) fix broken git compilation on mac
    Signed-off-by: Francis Begyn <francis@begyn.be>

--- users/francis/hm/configurations/git.nix
+++ users/francis/hm/configurations/git.nix
@@ -3,7 +3,7 @@
 {
   home.packages = with pkgs; [ git-crypt gh ];
   programs.git = {
-    package = pkgs.unstable.gitAndTools.gitFull;
+    package = pkgs.gitAndTools.gitFull;
     enable = true;
     aliases = {
       identity = "! git config user.name \"$(git config user.$1.name)\"; git config user.email \"$(git config user.$1.email)\"; git config user.signingkey \"$(git config user.$1.signingkey)\"; :";
