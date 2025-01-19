7e85b7d3be1a46d7c5fabf02630fef8b0b17ae7c
Francis Begyn <francis@begyn.be>
2025-01-19 17:57:58 +0100

    (cloud) add options for legacy and uefi
    Signed-off-by: Francis Begyn <francis@begyn.be>

--- lib/cloud.nix
+++ lib/cloud.nix
@@ -1,7 +1,7 @@
 { nixpkgs, ... }:
 
 {
-  mkCloudBox = hostname: {
+  mkCloudUEFI = hostname: {
     system ? "x86_64-linux",
     timeZone ? "Europe/Brussels",
     extraModules,
@@ -20,6 +20,65 @@
         boot.loader.grub.enable = false;
         boot.loader.systemd-boot.enable = true;
         boot.loader.efi.canTouchEfiVariables = true;
+
+        # set the hostname for the cloud box
+        networking.hostName = "${hostname}";
+        networking.useDHCP = false;
+        time.timeZone = "${timeZone}";
+
+        # enable tailscale by default in client mode
+        services.tailscale = {
+          enable = tailscale.enable;
+          openFirewall = false;
+          useRoutingFeatures = "${tailscale.routingMode}";
+          extraUpFlags = tailscale.extraUpFlags;
+        };
+
+        # enable firewall, we're online on the scary internet after all
+        networking.firewall.enable = true;
+
+        # we want ssh
+        services.openssh = {
+          enable = true;
+          openFirewall = false;
+        };
+
+        # Enable node exporter on all cloud servers
+        services.prometheus.exporters.node.enable = true;
+        services.prometheus.exporters.node.enabledCollectors = [ "systemd" ];
+
+        # default nix settings
+        nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
+        nixpkgs.config.allowUnfree = true;
+
+        home-manager.users.francis.home.stateVersion = "24.11";
+        system.stateVersion = "24.05"; # Did you read the comment?
+      })
+    ] ++ extraModules;
+  };
+  mkCloudGrub = hostname: {
+    system ? "x86_64-linux",
+    timeZone ? "Europe/Brussels",
+    extraModules,
+    tailscale ? {
+      enable = true;
+      routingMode = "client";
+      extraUpFlags = [
+        "--advertise-tags=tag:prod,tag:cloud"
+      ];
+    }
+  }: nixpkgs.lib.nixosSystem rec {
+    inherit system;
+    modules = [
+      ({config, pkgs, ...}: {
+        # configure boot through systemd-boot
+        boot.loader.grub.enable = true;
+        boot.loader.grub.efiSupport = true;
+        boot.loader.grub.device = "/dev/sda";
+        boot.loader.efi.efiSysMountPoint = "/boot";
+        boot.loader.efi.canTouchEfiVariables = true;
+
+        environment.systemPackages = with pkgs; [ efibootmgr os-prober ];
 
         # set the hostname for the cloud box
         networking.hostName = "${hostname}";
