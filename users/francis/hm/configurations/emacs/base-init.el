abce5827065242182ac6ed201b82fb1fe9b494a6
Francis Begyn <francis@begyn.be>
2026-06-08 18:30:12 +0200

    emacs: fix shell loading from exec
    Signed-off-by: Francis Begyn <francis@begyn.be>

--- users/francis/hm/configurations/emacs/base-init.el
+++ users/francis/hm/configurations/emacs/base-init.el
@@ -265,7 +265,7 @@ ARG filename to open"
   :config
   ;; Use bash in login-only mode so we skip the interactive zsh stack
   ;; (which exec's fish and trips `setopt zle` in non-interactive context).
-  (setq exec-path-from-shell-shell-name "/bin/bash"
+  (setq exec-path-from-shell-shell-name "bash"
         exec-path-from-shell-arguments '("-l"))
   (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE" "NIX_SSL_CERT_FILE" "NIX_PATH"))
     (add-to-list 'exec-path-from-shell-variables var))
